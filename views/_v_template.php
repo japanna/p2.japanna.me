40162b40fad35e9bee17253cece3b03eee6a5cea
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-10-21 14:57:09 -0500

    Updates up until class 7

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,13 +4,38 @@
 	<title><?php if(isset($title)) echo $title; ?></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
+
+	<!-- Common CSS/JSS -->
+    <link rel="stylesheet" href="/css/kramer.css">
+
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
 <body>	
+
+	<div id='menu'>
+
+        <a href='/'>Home</a>
+
+        <!-- Menu for users who are logged in -->
+        <?php if($user): ?>
+
+            <a href='/users/logout'>Logout</a>
+            <a href='/users/profile'>Profile</a>
+
+        <!-- Menu options for users who are not logged in -->
+        <?php else: ?>
+
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+
+        <?php endif; ?>
+
+    </div>
+
+    <br>
 
 	<?php if(isset($content)) echo $content; ?>
 
