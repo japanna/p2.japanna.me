6026c2063dc0c592f930cb58ad41d5307c314031
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-11-05 15:54:24 -0600

    minor change in copy

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,6 +1,6 @@
 <div id='users_profile'>
 	<h2><?=$user->first_name?>'s Ticker profile</h2>
-	<h3>Update ticker picture</h3>
+	<h3>Update Ticker picture</h3>
 	<img src="/uploads/avatars/<?=$user->img?>" alt="User profile" width="80" height="80">
 	<form method='POST' enctype="multipart/form-data" action='/users/p_upload/'>
 		<input type='file' name='photo'><br>
