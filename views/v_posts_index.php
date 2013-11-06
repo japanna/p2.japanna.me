bcd349d774cf186bafec18fa86383a5fbfab774b
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-11-05 20:29:28 -0600

    changed multiples of an ID to class

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -2,7 +2,7 @@
 	<h1>Ticks</h1>	
 	<?php foreach($posts as $post): ?>
 	<article>
-		<img id="profile_pic" src="/uploads/avatars/<?=$post['img']?>" alt="<?=$post['first_name']?> <?=$post['last_name']?>" width="60" height="60">
+		<img class="profile_pic" src="/uploads/avatars/<?=$post['img']?>" alt="<?=$post['first_name']?> <?=$post['last_name']?>" width="60" height="60">
     	<h2>$<?=$post['ticker_name']?> <span>(<?=$post['first_name']?>)</span>
     	<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
         <?=Time::display($post['created'])?>
